92c05c860a1a15ea8b247e6f58142a1f0ece2703
darianaren <darianarengifo@gmail.com>
2026-07-03 13:17:46 -0300

    app rutes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,32 @@
-import { useColorScheme } from 'react-native';
 import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from 'expo-router';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { useThemeColor } from '@/hooks/useThemeColor';
+
 import '../global.css';
 
-export default function Layout() {
+export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const backgroundColor = useThemeColor({}, 'background');
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack />
+      <Stack
+        screenOptions={{
+          headerShadowVisible: false,
+          contentStyle: {
+            backgroundColor: backgroundColor,
+          },
+          headerStyle: {
+            backgroundColor: backgroundColor,
+          },
+        }}>
+        <Stack.Screen
+          name="index"
+          options={{
+            title: '',
+          }}
+        />
+      </Stack>
     </ThemeProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,49 @@
-import ThemedText from '@/presentation/shared/ThemedText';
+import { animationMenuRoutes, menuRoutes, uiMenuRoutes } from '@/constants/Routes';
+import MenuItem from '@/presentation/menu/MenuItem';
 import ThemedView from '@/presentation/shared/ThemedView';
-import { Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function Home() {
+import { View } from 'react-native';
+const ComponentsApp = () => {
   return (
-    <ThemedView>
-      <ThemedText className="text-3xl">Hola mundo</ThemedText>
+    <ThemedView margin>
+      {animationMenuRoutes.map((route, index) => (
+        <MenuItem
+          key={route.title}
+          title={route.title}
+          icon={route.icon as keyof typeof Ionicons.glyphMap}
+          name={route.name}
+          isFirst={index === 0}
+          isLast={index === animationMenuRoutes.length - 1}
+        />
+      ))}
+
+      <View className="my-3" />
+
+      {uiMenuRoutes.map((route, index) => (
+        <MenuItem
+          key={route.title}
+          title={route.title}
+          icon={route.icon as keyof typeof Ionicons.glyphMap}
+          name={route.name}
+          isFirst={index === 0}
+          isLast={index === uiMenuRoutes.length - 1}
+        />
+      ))}
+
+      <View className="my-3" />
+
+      {menuRoutes.map((route, index) => (
+        <MenuItem
+          key={route.title}
+          title={route.title}
+          icon={route.icon as keyof typeof Ionicons.glyphMap}
+          name={route.name}
+          isFirst={index === 0}
+          isLast={index === menuRoutes.length - 1}
+        />
+      ))}
     </ThemedView>
   );
-}
+};
+export default ComponentsApp;
